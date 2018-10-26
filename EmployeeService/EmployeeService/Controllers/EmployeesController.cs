89f7f9aa13c586bdf2a0f2861a1ec56b2d1105a3
pmahesh6343@gmail.com <pmahesh6343@gmail.com>
2018-10-26 22:48:23 +0530

    Examles

---
+++ EmployeeService/EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace EmployeeService.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        /*
+         [HttpGet]
+        public IEnumerable<Employee> HelloCustomeReq() //custome requests
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+         */
+
+        
+         public HttpResponseMessage Get(string gender="All") 
+         {
+             using (EmployeeDBEntities entities = new EmployeeDBEntities())
+             {
+                 switch(gender.ToLower())
+                 {
+                     case "all":
+                         return Request.CreateResponse(HttpStatusCode.OK, entities.Employees.ToList());
+                     case "male":
+                         return Request.CreateResponse(HttpStatusCode.OK, entities.Employees.Where(e=>e.Gender.ToLower()=="male").ToList());
+                     case "female":
+                         return Request.CreateResponse(HttpStatusCode.OK, entities.Employees.Where(e => e.Gender.ToLower() == "female").ToList());
+                     default:return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Value for gender must be All, Male,Female " + gender + " is invalid");
+                 }
+             }
+
+         }
+
+     /*   public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+
+        public HttpResponseMessage Get(int id)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+
+                if(entity != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK,entity);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with ID = " + id.ToString()+" Not Found");
+                }
+            }
+        }
+
+    */
+        public HttpResponseMessage Post([FromBody]Employee employee)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    entities.Employees.Add(employee);
+                    entities.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    message.Headers.Location = new Uri(Request.RequestUri + employee.ID.ToString());
+                    
+                    return message;
+                }
+               
+            }
+            catch(Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, e);
+            }
+           
+        }
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+                    if (entity == null)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.NotFound, "Employee with ID = " + id.ToString() + " Not Found to Delete");
+                    }
+                    else
+                    {
+                        entities.Employees.Remove(entity);
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+
+                }
+            }
+            catch(Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, e);
+            }
+        }
+
+        public HttpResponseMessage Put(int id,[FromBody]Employee employee)
+        {
+            try
+            {
+
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with Id = " + id.ToString() + " Not Found to Update");
+                    }
+                    else
+                    {
+                        entity.FirstName = employee.FirstName;
+                        entity.LastName = employee.LastName;
+                        entity.Gender = employee.Gender;
+                        entity.Salary = employee.Salary;
+
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK, entity);
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, e);
+            }
+        }
+    }
+}
